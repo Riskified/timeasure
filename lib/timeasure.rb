6a9130da09661f316297ed1dd66f78255a30ebf7
Eliav Lavi <eliav@riskified.com>
2017-12-31 00:10:02 +0200

    added configurable enable-proc

--- lib/timeasure.rb
+++ lib/timeasure.rb
@@ -18,8 +18,7 @@ module Timeasure
       instance_interceptor = const_set(instance_interceptor_name_for(base_class), interceptor_module_for(base_class))
       class_interceptor = const_set(class_interceptor_name_for(base_class), interceptor_module_for(base_class))
 
-      # TODO: needs to be solved, perhaps a #timeasure_enabled? method is needed, maybe an extra configuration proc
-      # return if Rails.env.test? && disable_testing?
+      return unless timeasure_enabled?
 
       base_class.prepend instance_interceptor
       base_class.singleton_class.prepend class_interceptor
@@ -45,8 +44,8 @@ module Timeasure
       end
     end
 
-    # def disable_testing?
-    #   true
-    # end
+    def timeasure_enabled?
+      configuration.enable_timeasure_proc.call
+    end
   end
 end
--- lib/timeasure/configuration.rb
+++ lib/timeasure/configuration.rb
@@ -1,10 +1,11 @@
 module Timeasure
   class Configuration
-    attr_accessor :post_measuring_proc, :rescue_proc
+    attr_accessor :post_measuring_proc, :rescue_proc, :enable_timeasure_proc
 
     def initialize
       @post_measuring_proc = lambda { |base_class_name, method_name, t0, t1| }
       @rescue_proc = lambda { |e, base_class| }
+      @enable_timeasure_proc = lambda { true }
     end
   end
 end

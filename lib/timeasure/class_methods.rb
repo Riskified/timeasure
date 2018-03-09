0732ab6d9431036bbe66c0223830657c927ba032
Eliav Lavi <eliav@riskified.com>
2018-03-09 13:32:57 +0200

    sketched out possible beginning of a solution

--- lib/timeasure/class_methods.rb
+++ lib/timeasure/class_methods.rb
@@ -1,16 +1,14 @@
 module Timeasure
   module ClassMethods
     def tracked_instance_methods(*method_names)
-      instance_interceptor = const_get("#{timeasure_name}InstanceInterceptor")
       method_names.each do |method_name|
-        add_method_to_interceptor(instance_interceptor, method_name)
+        add_method_to_interceptor(interceptor: instance_interceptor, method_name: method_name)
       end
     end
 
     def tracked_class_methods(*method_names)
-      class_interceptor = const_get("#{timeasure_name}ClassInterceptor")
       method_names.each do |method_name|
-        add_method_to_interceptor(class_interceptor, method_name)
+        add_method_to_interceptor(interceptor: class_interceptor, method_name: method_name)
       end
     end
 
@@ -20,14 +18,24 @@ module Timeasure
 
     private
 
-    def add_method_to_interceptor(interceptor, method_name)
+    def add_method_to_interceptor(interceptor:, method_name:, private_method: false)
       interceptor.class_eval do
         define_method method_name do |*args, &block|
           Timeasure.measure(klass_name: interceptor.klass_name.to_s, method_name: method_name.to_s) do
             super(*args, &block)
           end
         end
+
+        private method_name if private_method
       end
+    end
+
+    def instance_interceptor
+      const_get("#{timeasure_name}InstanceInterceptor")
+    end
+
+    def class_interceptor
+      const_get("#{timeasure_name}ClassInterceptor")
     end
   end
 end
